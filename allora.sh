0d52b900acc718f843d43a4e0c2ece2ba5ad6342
snoopfear <39308472+snoopfear@users.noreply.github.com>
2024-08-20 19:38:08 +0300

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -90,8 +90,10 @@ while true; do
 
             log_message "Установка Allora Worker..."
             run_command "cd \$HOME"
-            run_command "git clone https://github.com/allora-network/basic-coin-prediction-node"
-            run_command "cd basic-coin-prediction-node"
+            run_command "git clone https://github.com/allora-network/allora-huggingface-walkthrough"
+            run_command "cd allora-huggingface-walkthrough"
+            run_command "mkdir -p worker-data"
+            run_command "chmod -R 777 worker-data"
 
             rm -rf config.json
             
@@ -104,50 +106,207 @@ while true; do
     "wallet": {
         "addressKeyName": "testkey",
         "addressRestoreMnemonic": "$seed_phrase",
-        "alloraHomeDir": "",
+        "alloraHomeDir": "/root/.allorad",
         "gas": "1000000",
         "gasAdjustment": 1.0,
-        "nodeRpc": "https://sentries-rpc.testnet-1.testnet.allora.network/",
+        "nodeRpc": "https://allora-testnet-1-rpc.testnet.nodium.xyz/",
         "maxRetries": 1,
         "delay": 1,
         "submitTx": false
     },
     "worker": [
         {
-            "topicId": 1,
-            "inferenceEntrypointName": "api-worker-reputer",
-            "loopSeconds": 5,
-            "parameters": {
-                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
-                "Token": "ETH"
-            }
-        },
-        {
-            "topicId": 2,
-            "inferenceEntrypointName": "api-worker-reputer",
-            "loopSeconds": 5,
-            "parameters": {
-                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
-                "Token": "ETH"
-            }
-        },
-        {
-            "topicId": 7,
-            "inferenceEntrypointName": "api-worker-reputer",
-            "loopSeconds": 5,
-            "parameters": {
-                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
-                "Token": "ETH"
-            }
-        }
-    ]
+           "topicId": 1,
+           "inferenceEntrypointName": "api-worker-reputer",
+           "loopSeconds": 1,
+           "parameters": {
+               "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+               "Token": "ETH"
+           }
+       },
+       {
+           "topicId": 3,
+           "inferenceEntrypointName": "api-worker-reputer",
+           "loopSeconds": 5,
+           "parameters": {
+               "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+               "Token": "BTC"
+           }
+       },
+       {
+           "topicId": 5,
+           "inferenceEntrypointName": "api-worker-reputer",
+           "loopSeconds": 4,
+           "parameters": {
+               "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+               "Token": "SOL"
+           }
+       },
+       {
+           "topicId": 7,
+           "inferenceEntrypointName": "api-worker-reputer",
+           "loopSeconds": 2,
+           "parameters": {
+               "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+               "Token": "ETH"
+           }
+       },
+       {
+           "topicId": 8,
+           "inferenceEntrypointName": "api-worker-reputer",
+           "loopSeconds": 3,
+           "parameters": {
+               "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+               "Token": "BNB"
+           }
+       },
+       {
+           "topicId": 9,
+           "inferenceEntrypointName": "api-worker-reputer",
+           "loopSeconds": 5,
+           "parameters": {
+               "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+               "Token": "ARB"
+           }
+       }
+       
+   ]
 }
+EOF
+
+            rm -rf app.py
+            
+            # Запрос Api
+            read -p "Введите ваш API: " api_coin
+
+            # Создание нового файла app.py
+            cat <<EOF > app.py
+            
+from flask import Flask, Response
+import requests
+import json
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from sklearn.preprocessing import MinMaxScaler
+
+# create our Flask app
+app = Flask(__name__)
+
+def get_coingecko_url(token):
+    base_url = "https://api.coingecko.com/api/v3/coins/"
+    token_map = {
+        'ETH': 'ethereum',
+        'SOL': 'solana',
+        'BTC': 'bitcoin',
+        'BNB': 'binancecoin',
+        'ARB': 'arbitrum'
+    }
+    
+    token = token.upper()
+    if token in token_map:
+        url = f"{base_url}{token_map[token]}/market_chart?vs_currency=usd&days=1&interval=minute"
+        return url
+    else:
+        raise ValueError("Unsupported token")
+
+def prepare_data(data, time_step=10):
+    """Prepare the data for LSTM model."""
+    x, y = [], []
+    for i in range(len(data) - time_step - 1):
+        x.append(data[i:(i + time_step), 0])
+        y.append(data[i + time_step, 0])
+    return np.array(x), np.array(y)
+
+def build_lstm_model(input_shape):
+    """Build and return an LSTM model."""
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(50, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
+@app.route("/inference/<string:token>")
+def get_inference(token):
+    """Generate inference for given token."""
+    try:
+        # Get the data from Coingecko
+        url = get_coingecko_url(token)
+    except ValueError as e:
+        return Response(json.dumps({"error": str(e)}), status=400, mimetype='application/json')
+
+    headers = {
+        "accept": "application/json",
+        "x-cg-demo-api-key": "$api_coin"  # Replace with your API key
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        df = pd.DataFrame(data["prices"])
+        df.columns = ["ds", "y"]
+        df["ds"] = pd.to_datetime(df["ds"], unit='ms')
+        df = df[["ds", "y"]]
+        print(df.tail(5))
+    else:
+        return Response(json.dumps({"Failed to retrieve data from the API": str(response.text)}),
+                        status=response.status_code,
+                        mimetype='application/json')
+
+    # Preprocess the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(df["y"].values.reshape(-1, 1))
+
+    time_step = 10
+    x, y = prepare_data(scaled_data, time_step)
+
+    # Reshape data for LSTM (samples, time steps, features)
+    x = x.reshape(x.shape[0], x.shape[1], 1)
+
+    # Build the LSTM model
+    model = build_lstm_model((x.shape[1], 1))
+
+    # Train the model
+    model.fit(x, y, batch_size=1, epochs=10, verbose=1)
+
+    # Make a prediction for the next 10 or 20 minutes
+    last_sequence = scaled_data[-time_step:]
+    last_sequence = last_sequence.reshape(1, time_step, 1)
+    forecasted_value = model.predict(last_sequence)
+    forecasted_value = scaler.inverse_transform(forecasted_value)[0][0]
+
+    print(forecasted_value)  # Print the forecasted value
+
+    return Response(str(forecasted_value), status=200)
+
+# run our Flask app
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+EOF
+
+            rm -rf requirements.txt
+            
+            # Создание нового файла requirements.txt
+            cat <<EOF > requirements.txt
+flask[async]
+gunicorn[gthread]
+transformers[torch]
+pandas
+torch==2.0.1 
+python-dotenv
+requests==2.31.0
+plotly
+prophet
 EOF
 
             log_message "Запуск Allora Worker..."
             chmod +x init.config
             ./init.config
-            cd ~/basic-coin-prediction-node
+            cd ~/allora-huggingface-walkthrough
+            
             docker compose up -d --build
             ;;
         2)
